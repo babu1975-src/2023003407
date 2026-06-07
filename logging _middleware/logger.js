8fbbab381649b6b3e99f47478a72f9c86c6abee3
babu1975-src <anildaggubati1975@gmail.com>
2026-06-07 11:15:51 +0530

    Update logger.js

--- logging _middleware/logger.js
+++ logging _middleware/logger.js
@@ -1,1 +1,18 @@
-log(stack, level, packageName, message)
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+fetch("http://4.224.186.213/evaluation-service/logs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  },
+  body: JSON.stringify({
+    stack: "frontend",
+    level: "info",
+    package: "component",
+    message: "Testing logger"
+  })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error("Error:", error));
